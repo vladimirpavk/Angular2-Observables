0110112c8d713af34072056ce0ad1c60094049e7
Vladimir Pavković <vladimirpavk@gmail.com>
2016-12-15 13:52:42 +0100

    Initial commit

---
+++ app/ts/app.component.ts
@@ -0,0 +1,8 @@
+/// <reference path="../../node_modules/@angular/common/index.d.ts" />
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app/ts/app.component.html'
+})
+export class AppComponent{
+}
